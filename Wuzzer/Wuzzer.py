bb58206cfd560d4546f756612bbdfed06a165b64
Mahdi Heidari <mahdih3idari@gmail.com>
2021-07-10 19:31:53 +0430

    Update Wuzzer.py

--- Wuzzer/Wuzzer.py
+++ Wuzzer/Wuzzer.py
@@ -40,30 +40,53 @@ def Session_Creator(login_url, payload={}):
 
 
 def main():
-    """import argparse
-    parser = argparse.ArgumentParser(description="Link Extractor Tool with Python")
-    parser.add_argument("url", help="The URL to extract links from.")
-    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 30.", default=30, type=int)
-    
+    import argparse
+    parser = argparse.ArgumentParser(description="Simple Web Fuzzer")
+    parser.add_argument("-i", "--login", help="login page url")
+    parser.add_argument("-o", "--logout", help="logout page url")
+    parser.add_argument("-u", "--username", help="username")
+    parser.add_argument("-p", "--password", help="password")
+    parser.add_argument("-t", "--thread", help="Number of thread", default=1, type=int)
+    parser.add_argument("-mu", "--maxUrl", help="Number of max URLs to crawl, default is 30.", default=30, type=int)
+    parser.add_argument('-x', '--XSSi', help="Xss injecyion attack", action='store_true', default=False)
+    parser.add_argument('-xp', '--XSSpayload', help="Xss injecyion payload path")
+    parser.add_argument('-s', '--SQLi', help="SQL injecyion attack", action='store_true', default=False)
+    parser.add_argument('-sp', '--SQLpayload', help="SQL injecyion payload path")
+    parser.add_argument('-c', '--CMDi', help="Command injecyion attack", action='store_true', default=False)
+    parser.add_argument('-cp', '--CMDpayload', help="Command injecyion payload path")
+    parser.add_argument('--test', help="test on DVWA", action='store_true', default=False)
     args = parser.parse_args()
-    url = args.url
-    max_urls = args.max_urls """
 
-    loginURL  = "http://127.0.0.1/login.php"
-    logoutURL = "http://127.0.0.1/logout.php"
+    loginURL  = args.login
+    logoutURL  = args.logout
+    thread = args.thread
+    max_urls = args.maxUrl 
+
+    if args.test :
+        loginURL  = "http://127.0.0.1/login.php"
+        logoutURL = "http://127.0.0.1/logout.php"
+
+    username = args.username if args.username else 'admin'
+    password = args.password if args.password else 'password'
 
     # Fill in your details here to be posted to the login form.
     payload = { 
-        'username': 'admin',
-        'password': 'password',
+        'username': username,
+        'password': password,
         'Login': 'Login'
     }
 
     Session, protectedURL = Session_Creator(loginURL, payload)
 
-    internal_urls = Crawler(Session, protectedURL, loginURL, logoutURL).crawl(max_urls=30, DynamicSite=0, verbose=False)
+    internal_urls = Crawler(Session, protectedURL, loginURL, logoutURL).crawl(max_urls, DynamicSite=0, verbose=False)
 
-    XssInjection(Session, "./payload/xss-payload-list.txt", internal_urls)
+    #print(internal_urls)
+    if args.XSSi :
+        XssInjection(Session, args.XSSpayload, internal_urls).Fuzzer()
+    if args.SQLi :
+        XssInjection(Session, args.SQLpayload, internal_urls).Fuzzer()
+    if args.CMDi :
+        XssInjection(Session, args.CMDpayload, internal_urls).Fuzzer()
     
 if __name__ == '__main__':
     main()
