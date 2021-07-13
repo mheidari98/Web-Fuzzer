8fd885e3335ab546defed4842e38cac5569f1b76
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-07-13 02:16:08 -0400

    Update SqlInjection.py

--- Wuzzer/SqlInjection.py
+++ Wuzzer/SqlInjection.py
@@ -10,23 +10,23 @@ class SqlInjection(Injection):
             self.payloads  = self.Get_payloads('payload/sqli_min.txt')
 
     def CheckFault(self, payload, response_html_doc):
-        err_list = ['syntax']
+        err_list = ['You have an error in your SQL syntax']
         for err in err_list:
             if err in response_html_doc: return True
         return False
 
     def PayloadInjection(self, params, selected_input, url, href, formMethod):
-        for payload in self.payloads:
-            params_dict = {}
-            for i in range(len(params)):
-                params_dict[params.loc[i, 'name']] = params.loc[i, 'value']
+        params_dict = {}
+        for i in range(len(params)):
+            params_dict[params.loc[i, 'name']] = params.loc[i, 'value']
 
+        for payload in self.payloads:
             params_dict[params.loc[selected_input, 'name']] = payload
-            #new_URL = add_url_params(href, params)
+            
             response_html_doc = self.send_request(href, params_dict, formMethod)
 
             fault = self.CheckFault(payload, response_html_doc)
             if fault: 
-                self.PrintErr("SQL Injection", payload, href)
+                self.PrintErr("SQL Injection", href, params.loc[selected_input, 'name'], payload)
                 return True
         return False
